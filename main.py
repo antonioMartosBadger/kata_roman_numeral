a0b275e266bb7573aceee21f58f1c445d150dea1
Antonio Martos Rodríguez <antonio.martos@badgermapping.com>
2022-07-15 11:41:42 +0200

    Covering 2 first cases

---
+++ main.py
@@ -0,0 +1,5 @@
+def convert(number: int) -> str:
+    if number == 1:
+        return 'I'
+    else:
+        return 'II'
--- test/exercise_test.py
+++ test/exercise_test.py
@@ -1,7 +1,16 @@
 import unittest
 
+from main import convert
+
 
 class TestExercise(unittest.TestCase):
 
-    def test_example(self):
-        self.assertEqual(True, True)
+    def test_given_the_number_1_when_converting_to_roman_then_I_is_returned(self):
+        result = convert(1)
+
+        self.assertEqual('I', result)
+
+    def test_given_the_number_2_when_converting_to_roman_then_II_is_returned(self):
+        result = convert(2)
+
+        self.assertEqual('II', result)
