fb67c1a4f28779e5922994da1f105343cbe181e0
Antonio Martos Rodríguez <antonio.martos@badgermapping.com>
2022-07-15 11:57:35 +0200

    Refactoring

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+
+UNIQUE_VALUES = {
+    1: 'I',
+    5: 'V'
+}
+
 def convert(number: int) -> str:
-    if number == 5:
-        return 'V'
-    elif number == 4:
+    if number in UNIQUE_VALUES:
+        return UNIQUE_VALUES[number]
+    else:
+        return calculate_conversion(number)
+        
+def calculate_conversion(number: int) -> str:
+    if number == 4:
         return 'IV'
     return 'I'*number
